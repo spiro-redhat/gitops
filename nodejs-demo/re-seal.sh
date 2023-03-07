b7c3d68f0c6852e4012b1f047550467a7e6590a2
Spiro Stathakis <spiro@redhat.com>
2023-03-07 13:49:13 +0000

    Moving into apps folder

--- nodejs-demo/re-seal.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash 
-kubeseal --controller-namespace=sealed-secrets -n nodejs-demo < secret.txt > sealed-secret.yaml
-
--- openshift-gitops/unseal.sh
+++
@@ -1,6 +0,0 @@
-oc get secret -n sealed-secrets -l sealedsecrets.bitnami.com/sealed-secrets-key -o yaml >/tmp/ss-cert
-
-kubeseal -o yaml --recovery-unseal --recovery-private-key /tmp/ss-cert < repo-1028610631-sealed.yaml \
-> repo-1028610631.yaml
-
-rm /tmp/ss-cert
